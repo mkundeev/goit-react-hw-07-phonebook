583374dd0920c6eea907e35168ec4901c3b86306
Maksym Kundeev <mkundeev@gmail.com>
2022-07-09 17:43:46 +0300

    creatSelector for filter, npm axios

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,10 +1,10 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
 
-export const getFilteredContacts = state => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) =>
+    contacts.filter(contact => contact.name.toLowerCase().includes(filter))
+);
